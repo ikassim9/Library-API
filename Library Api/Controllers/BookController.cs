8e56261c4c812d0af148029842a9de3d21d85835
Ismail Kassim <ikassim9@gmail.com>
2023-05-04 14:29:18 -0500

    added support for updating book

--- Library Api/Controllers/BookController.cs
+++ Library Api/Controllers/BookController.cs
@@ -13,7 +13,7 @@ namespace Library_Api.Controllers
     {
         private readonly IBookService _bookService;
         private readonly IConfiguration _config;
-        private readonly IS3Service _s3;      
+        private readonly IS3Service _s3;
         public BookController(IBookService bookService, IConfiguration config, IS3Service s3)
         {
             _bookService = bookService;
@@ -59,7 +59,7 @@ namespace Library_Api.Controllers
             var fileExt = Path.GetExtension(file.FileName);
             var objName = $"{Guid.NewGuid()}.{fileExt}";
             var bucketName = _config["AwsConfiguration:BucketName"];
-    
+
 
             var s3Obj = new S3Object()
             {
@@ -78,7 +78,7 @@ namespace Library_Api.Controllers
             var result = await _s3.UploadFileAsync(s3Obj, creds);
 
 
-            if(result.StatusCode == 200)
+            if (result.StatusCode == 200)
             {
 
                 book.BookCover = $"https://{bucketName}.s3.amazonaws.com/{objName}";
@@ -92,28 +92,66 @@ namespace Library_Api.Controllers
                 return StatusCode(StatusCodes.Status500InternalServerError);
             }
 
-          
-   
+
+
         }
 
         [HttpPut("{bookId}")]
-        public ActionResult<Book> UpdateBook(string bookId, [FromBody] Book book)
+        public async Task<ActionResult<Book>> UpdateBook(string bookId, [FromForm] Book book)
         {
             Book existingBook = _bookService.Get(bookId);
-            
-            if(existingBook == null)
+
+            if (existingBook == null)
             {
                 return NotFound($"Book with id {bookId} doesn' exist");
             }
 
-            _bookService.UpdateBook(bookId, book);
+            if (!string.IsNullOrEmpty(book.BookCover))
+            {
+                _bookService.UpdateBook(bookId, book);
 
+            }
+            else
+            {
+                var file = Request.Form.Files[0];
+                await using var memoryStream = new MemoryStream();
+                await file.CopyToAsync(memoryStream);
+                var fileExt = Path.GetExtension(file.FileName);
+                var objName = $"{Guid.NewGuid()}.{fileExt}";
+                var bucketName = _config["AwsConfiguration:BucketName"];
+
+                var s3Obj = new S3Object()
+                {
+                    BucketName = bucketName,
+                    InputStream = memoryStream,
+                    Name = objName
+                };
+
+                var creds = new AwsCredentials()
+                {
+                    AwsAcessKey = _config["AwsConfiguration:AcessKey"],
+                    AwsSecretKey = _config["AwsConfiguration:SecretKey"],
+
+                };
+
+                await _s3.DeleteFileAsync(existingBook.BookCoverUrl, bucketName, creds);
+                var result = await _s3.UploadFileAsync(s3Obj, creds);
+
+                if (result.StatusCode == 200)
+                {
+
+                    book.BookCover = $"https://{bucketName}.s3.amazonaws.com/{objName}";
+                    book.BookCoverUrl = objName;
+                    _bookService.UpdateBook(bookId, book);
+                }
+
+            }
             return NoContent();
-
-
         }
 
-        [HttpDelete("{bookId}")]
+   
+
+    [HttpDelete("{bookId}")]
         public async Task<ActionResult<Book>> DeleteBook(string bookId)
         {
             Book existingBook = _bookService.Get(bookId);
